bef9ff216891c28a8809a088323ad41e5aa8024a
kgagne888 <kgagne888@gmail.com>
2025-07-23 13:14:26 -0600

    Add resize grid button with functionality

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const container = document.querySelector(".container");
-// TODO: There has to be a better way to calculate this
-container.style.width = `${16 * 20}px`;
+const gridButton = document.querySelector("#size-grid");
 
-
-// add 16x16 square divs to the container 
-for (let i=0; i<(16*16); i++) {
-    const square = document.createElement("div");
-    square.id = `square${i}`;
-    square.className = 'square';
-    container.appendChild(square);
-    square.addEventListener("mouseenter", () => {
-        square.classList.add('hovered-square');
-    })
+function resizeGrid(gridSize) {
+    container.innerHTML = '';
+    for (let i=0; i<(gridSize*gridSize); i++) {
+        const square = document.createElement("div");
+        square.id = `square${i}`;
+        square.className = 'square';
+        square.style.width = `${(container.offsetWidth - 2) / gridSize}px`;
+        square.style.height = square.style.width;
+        container.appendChild(square);
+        square.addEventListener("mouseenter", () => {
+            square.classList.add('hovered-square');
+        })
+    }
 }
+
+// 16 is used as the initial
+resizeGrid(16);
+
+function promptGridSize() {
+    newGridSize = prompt("What grid size do you want?");
+    if (newGridSize > 100 || newGridSize < 0) {
+        alert("Grid size is not accurate. Choose a number below 100 and above 0");
+        return;
+    }
+    resizeGrid(newGridSize);   
+}
+
+gridButton.addEventListener("click", promptGridSize);
+
+
+
